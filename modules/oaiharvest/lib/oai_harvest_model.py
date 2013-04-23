7db27c5d190736a2856d95d168c73ffcc3987d39
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2013-04-23 08:46:00 +0200

    oairepository: automatically compute model field
    * Connects OaiREPOSITORY model to SQLAlchemy events to ensure field
      setDefinition is properly computed on db insert/update when using
      the SQLAlchemy model.

--- modules/oaiharvest/lib/oai_harvest_model.py
+++ modules/oaiharvest/lib/oai_harvest_model.py
@@ -23,10 +23,8 @@ Oai harvest database models.
 
 # General imports.
 from invenio.sqlalchemyutils import db
+from sqlalchemy import event
 
-# Create your models here.
-
-#from websearch_model import Collection
 from invenio.bibedit_model import Bibrec
 from invenio.bibsched_model import SchTASK
 
@@ -68,15 +66,49 @@ class OaiREPOSITORY(db.Model):
     setRecList = db.Column(db.iLargeBinary, nullable=True)
     last_updated = db.Column(db.DateTime, nullable=False,
                              server_default='1970-01-01 00:00:00')
-    p1 = db.Column(db.Text, nullable=False)
-    f1 = db.Column(db.Text, nullable=False)
-    m1 = db.Column(db.Text, nullable=False)
-    p2 = db.Column(db.Text, nullable=False)
-    f2 = db.Column(db.Text, nullable=False)
-    m2 = db.Column(db.Text, nullable=False)
-    p3 = db.Column(db.Text, nullable=False)
-    f3 = db.Column(db.Text, nullable=False)
-    m3 = db.Column(db.Text, nullable=False)
+    p1 = db.Column(db.Text, nullable=False, default='')
+    f1 = db.Column(db.Text, nullable=False, default='')
+    m1 = db.Column(db.Text, nullable=False, default='')
+    p2 = db.Column(db.Text, nullable=False, default='')
+    f2 = db.Column(db.Text, nullable=False, default='')
+    m2 = db.Column(db.Text, nullable=False, default='')
+    p3 = db.Column(db.Text, nullable=False, default='')
+    f3 = db.Column(db.Text, nullable=False, default='')
+    m3 = db.Column(db.Text, nullable=False, default='')
+
+    @classmethod
+    def update_setdefinition_listener(cls, mapper, connection, target):
+        """
+        Update the setDefinition attribute on before_insert/before_update
+        events.
+        """
+        # FIXME: Next two lines
+        op1 = ''
+        op2 = ''
+
+        # Set fields to empty string if none.
+        for attr in ['p1', 'f1', 'm1', 'p2', 'f2', 'm2', 'p3', 'f3', 'm3',
+                     'setCollection', 'setName', 'setSpec', 'setDescription']:
+            if getattr(target, attr) is None:
+                setattr(target, attr, '')
+
+        target.setDefinition = \
+            'c=' + target.setCollection + ';' + \
+            'p1=' + target.p1 + ';' + \
+            'f1=' + target.f1 + ';' + \
+            'm1=' + target.m1 + ';' + \
+            'op1=' + op1 + ';' + \
+            'p2=' + target.p2 + ';' + \
+            'f2=' + target.f2 + ';' + \
+            'm2=' + target.m2 + ';' + \
+            'op2=' + op2 + ';' + \
+            'p3=' + target.p3 + ';' + \
+            'f3=' + target.f3 + ';' + \
+            'm3=' + target.m3 + ';'
+
+# Connect Alchemy event listeners to update setDefinition on INSERT and UPDATE
+event.listen(OaiREPOSITORY, 'before_insert', OaiREPOSITORY.update_setdefinition_listener)
+event.listen(OaiREPOSITORY, 'before_update', OaiREPOSITORY.update_setdefinition_listener)
 
 
 class OaiHARVESTLOG(db.Model):
