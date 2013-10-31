a6fac7a326f60c20cd6c66bde0c552a6baf3871a
Jiri Kuncar <jiri.kuncar@cern.ch>
2013-10-31 16:43:58 +0100

    scripts: ported script for creating demosite

--- invenio/legacy/bibformat/scripts/bibreformat.py
+++ invenio/legacy/bibformat/scripts/bibreformat.py
@@ -1,7 +1,7 @@
-#!@PYTHON@
+# -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2008, 2010, 2011 CERN.
+## Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2010, 2011, 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -17,18 +17,15 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""
-BibDocFile CLI tool.
-"""
+"""Call BibFormat engine and create HTML brief (and other) formats for
+   bibliographic records.  Upload formats via BibUpload."""
 
 __revision__ = "$Id$"
 
-try:
-    from invenio.flaskshell import *
-    from invenio.bibdocfilecli import main
-except ImportError, e:
-    print "Error: %s" % e
-    import sys
-    sys.exit(1)
+from invenio.base.factory import with_app_context
 
-main()
+
+@with_app_context()
+def main():
+    from invenio.bibreformat import main as bibreformat_main
+    return bibreformat_main()
